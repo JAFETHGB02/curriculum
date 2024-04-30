1067ca94bdb736c2979769cd03ada4565d08bf23
JAFETHGB02 <jafethbejarano1715@gmail.com>
2024-04-30 16:03:42 -0600

    arreglo de botones

--- jafeth/java.js
+++ jafeth/java.js
@@ -1,7 +1,7 @@
-const like = document.getElementById('num1')
+const like = document.getElementById('btn btn-formulario btn-2')
 LikeCountElement = document.getElementById('num-0')
 DisLikeCountElement = document.getElementById('num0')
-let dislike = document.getElementById('num2')
+let dislike = document.getElementById('btn btn-formulario btn-2')
 let LikeCUENTA = 0;
 let LikeCuenta = 0;
 
