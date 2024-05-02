283ab717ed678314c1244142758d256a89ffe765
JAFETHGB02 <jafethbejarano1715@gmail.com>
2024-05-02 09:22:46 -0600

    reacomodacion de codigo

--- jafeth/java.js
+++ jafeth/java.js
@@ -1,6 +1,8 @@
 LikeCountElement = document.getElementById('btn-3')
 DisLikeCountElement = document.getElementById('btn-4')
+
 const like = document.getElementById('btn-5')
+
 let Dislike = document.getElementById('btn-6')
 let LikeCuenta = 0;
 let DisLikeCuenta = 0;
